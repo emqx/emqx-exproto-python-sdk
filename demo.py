3b1881a8f1a65e185c6863c5d6a820c537fe7197
YouST <adek06@163.com>
2020-08-22 16:08:13 +0800

    feat(demo): add abstract class and demo

---
+++ demo.py
@@ -0,0 +1,21 @@
+from abstract_handler import AbstractExProtoHandler
+from connection import Connection, ConnectionInfo
+
+
+class SdkDemo(AbstractExProtoHandler):
+    def on_connect(self, connection: Connection, connection_info: ConnectionInfo):
+        print(connection)
+        print(connection_info)
+
+    def on_received(self, connection: Connection, data: bytes, state: any):
+        print(connection)
+        print(bytes.decode(data))
+
+    def on_terminated(self, connection: Connection, reason: str, state: any):
+        print(connection)
+        print(reason)
+
+    def on_deliver(self, connection: Connection, message_list: list):
+        print(connection)
+        for message in message_list:
+            print(message)
