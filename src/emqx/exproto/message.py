225be6fe85346a54cb78f5da8a12655add4e252d
JianBo He <heeejianbo@gmail.com>
2020-09-04 11:25:55 +0800

    improve: Add LICENSE statement

--- src/emqx/exproto/message.py
+++ src/emqx/exproto/message.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2020 EMQ Technologies Co., Ltd. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from emqx.erlport.erlterms import Atom
 from .until import to_binary
 
@@ -60,7 +74,7 @@ class Message:
         return tuple_list
 
     def __str__(self):
-        string = f"EmqxDeliverMessage{{\n " \
+        string = f"Message{{\n " \
             f"id={self.idd},\n " \
             f"qos={self.qos},\n " \
             f"from={self.from_},\n " \
